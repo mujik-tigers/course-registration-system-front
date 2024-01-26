57522cb3bac1c658ce61318ca25e97f3976235bd
Hyun <ghkdgus29@naver.com>
2024-01-26 22:58:46 +0900

    feat: 수강 바구니 취소, 현재 학기와 년도 받아오기, 수강 바구니 담은 사람 수 받아오기 기능 구현

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -15,7 +15,9 @@
         <td>{{ value.hoursPerWeek }}</td>
         <td>{{ value.schedule }}</td>
         <td>{{ value.professorName }}</td>
-        <td><button class="basketPageButton">취소</button></td>
+        <td>
+          <button @click="cancelLecture(value.id, index)" class="basketPageButton">취소</button>
+        </td>
       </tr>
     </table>
   </div>
@@ -36,8 +38,10 @@
         <td>{{ value.targetGrade }}</td>
         <td>{{ value.departmentName }}</td>
         <td>{{ value.professorName }}</td>
-        <td>{{ value.applicants }}/{{ value.totalCapacity }}</td>
-        <td><button class="basketPageButton">새로고침</button></td>
+        <td>{{ applicants[index] }}/{{ value.totalCapacity }}</td>
+        <td>
+          <button @click="fetchApplicants(value.id, index)" class="basketPageButton">새로고침</button>
+        </td>
       </tr>
     </table>
     <div class="pagenation">
@@ -49,60 +53,106 @@
 
 <script>
 import axios from 'axios';
+
 axios.defaults.withCredentials = true;
 
 export default {
   name: "WebBasket",
   data() {
     return {
-      fetchStudentBasketUrl: "https://course-registration-system.site/baskets",
+      basketBaseUrl: "https://course-registration-system.site/baskets",
       basketCategories: ["NO.", "학년", "이수구분", "강의번호", "교과목명", "학점", "시간", "일정", "담당교수", "취소"],
       studentBasket: [],
       fetchLecturesUrl: "https://course-registration-system.site/lectures",
       lectureCategories: ["NO.", "강의번호", "이수구분", "교과목명", "학점", "시간", "일정", "대상학년", "개설학과", "교강사", "신청/정원", "새로고침"],
       lectures: [],
-      openingYear: 2024,
-      semester: 'FIRST',
+      openingYear: null,
+      semester: null,
+      applicants: []
     };
   },
   mounted() {
     this.fetchStudentBasket();
-    this.fetchLecutures();
+    this.fetchLectures();
   },
   methods: {
     fetchStudentBasket() {
       if (this.studentBasket.length === 0) {
-        axios.get(this.fetchStudentBasketUrl)
-        .then(res => {
-          if (res.data.code == 200) {
-            this.studentBasket = res.data.data.baskets
-          }
-        })
-        .catch(error => {
-            if (error.response && error.response.data.code == 401) {
-              alert('세션이 만료되어 로그아웃되었습니다');
-              window.location = '/';
-            }
-          }
-        )
+        axios.get(this.basketBaseUrl)
+            .then(res => {
+              if (res.data.code == 200) {
+                this.studentBasket = res.data.data.baskets;
+              }
+            })
+            .catch(error => {
+                  if (error.response && error.response.data.code == 401) {
+                    alert('세션이 만료되어 로그아웃되었습니다');
+                    window.location = '/';
+                  }
+                }
+            )
       }
     },
-    fetchLecutures() {
-      if (this.lectures.length === 0) {
-        axios.get(this.fetchLecturesUrl, {params:{openingYear: this.openingYear, semester: this.semester}})
-        .then(res => {
-          if (res.data.code == 200) {
-            this.lectures = res.data.data.lectures;
-          }
-        })
-        .catch(error => {
-            if (error.response && error.response.data.code == 401) {
-              alert('세션이 만료되어 로그아웃되었습니다');
-              window.location = '/';
-            }
-          }
-        )
+    async fetchLectures() {
+      if (this.openingYear === null || this.semester === null) {
+        await this.fetchCurrentYearAndSemester();
       }
+      if (this.lectures.length === 0) {
+        axios.get(this.fetchLecturesUrl, {params: {openingYear: this.openingYear, semester: this.semester}})
+            .then(res => {
+              if (res.data.code == 200) {
+                this.lectures = res.data.data.lectures;
+                this.applicants = Array.from({length: this.lectures.length}, () => null);
+              }
+            })
+            .catch(error => {
+                  if (error.response && error.response.data.code == 401) {
+                    alert('세션이 만료되어 로그아웃되었습니다');
+                    window.location = '/';
+                  }
+                }
+            )
+      }
+    },
+    cancelLecture(basketId, index) {
+      axios.delete(this.basketBaseUrl + "/" + basketId)
+          .then(res => {
+            if (res.data.code == 200) {
+              this.studentBasket.splice(index, 1);
+            }
+          })
+          .catch(error => {
+            if (error.response && error.response.data.code == 400) {
+              alert(error.response.data.message);
+            }
+          })
+    },
+    fetchApplicants(lectureId, index) {
+      axios.get(this.fetchLecturesUrl + "/" + lectureId + "/basket-count")
+          .then(res => {
+            if (res.data.code == 200) {
+              this.applicants[index] = res.data.data.currentBasketStoringCount;
+            }
+          })
+          .catch(error => {
+            if (error.response) {
+              alert(error.response.data.message);
+            }
+          });
+    },
+    async fetchCurrentYearAndSemester() {
+      await axios.get("https://course-registration-system.site/clock/current-year-and-semester")
+          .then(res => {
+            if (res.data.code == 200) {
+              this.openingYear = res.data.data.year;
+              this.semester = res.data.data.semester;
+            }
+          })
+          .catch(error => {
+            if (error.response) {
+              alert(error.response.data.message);
+            }
+          });
     },
   }
 }
